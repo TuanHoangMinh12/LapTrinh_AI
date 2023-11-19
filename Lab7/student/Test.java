773db0f1a394f22476c7673691e1b390bcab1b66
HoangMinhTuan <21130600@st.hcmuaf.edu.vn>
2023-11-19 22:11:04 +0700

    nop Lab7

---
+++ Lab7/student/Test.java
@@ -0,0 +1,10 @@
+package Lab7;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Node test = new Node();
+        test.generateBoard();
+        test.displayBoard();
+    }
+}
